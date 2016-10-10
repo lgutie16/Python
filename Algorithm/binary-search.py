25627ec0ea3e86751d5e9c56ffbc8da45ad8336d
lgutie16 <lgutie16.eafit.edu.co>
2016-10-09 22:30:18 -0500

    tutorship 20162

---
+++ Algorithm/binary-search.py
@@ -0,0 +1,21 @@
+arr = []
+def binary_search(low, high, key):
+	if low<=high:
+		mid=int((low+high)/2)
+		if key == arr[mid]:
+			return mid
+		elif key < arr[mid]:
+			high = mid-1
+		else:
+			low = mid+1
+		return binary_search(low, high, key)	
+	return -1
+
+def main():	
+  arr = [int(i) for i in raw_input().split()]
+  trgt =int( input())
+  print binary_search(0, len(arr), trgt)
+
+main()
+
+
